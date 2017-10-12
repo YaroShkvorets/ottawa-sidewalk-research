7c30ab864b4b418bc6c6ab898d59f63c5e3418a0
Yaro <shkvorets@gmail.com>
2017-10-12 15:13:50 -0400

    added checking for tag sidewalk=both

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ const rbush = require('geojson-rbush')
 
 const sidewalkTree = rbush()
 
-roadsWithSidewalks = [];
+roadsWithBothSidewalks = [];
 roadsWithLeftSidewalks = [];
 roadsWithRightSidewalks = [];
-roadsWithoutSidewalks = [];
+roadsWithNoSidewalks = [];
 roadsToSplit = [];
 
 const inPedDataPath = "data/ottawa_central_sidewalks.json"
 const inRoadDataPath = "data/ottawa_central_roads.json"
-const outRoadsWithSidewalksPath = "data/ottawa_central_roads_with_sidewalks.json"
-const outRoadsWithoutSidewalksPath = "data/ottawa_central_roads_without_sidewalks.json"
+const outRoadsWithBothSidewalksPath = "data/ottawa_central_roads_with_both_sidewalks.json"
+const outroadsWithNoSidewalksPath = "data/ottawa_central_roads_without_sidewalks.json"
 const outRoadsToSplitPath = "data/ottawa_central_roads_to_split.json"
 const outRoadsWithLeftSidewalksPath = "data/ottawa_central_roads_with_left_sidewalks.json"
 const outRoadsWithRightSidewalksPath = "data/ottawa_central_roads_with_right_sidewalks.json"
@@ -49,7 +49,7 @@ for (let road of roads) {
   let offset = kOffsetFromRoadEnd;
   let foundSidewalk=false
   let pointsTotal=0;
-  let pointsWithSidewalk=0, pointsWithLeftSidewalk=0, pointsWithRightSidewalk=0;
+  let pointsWithBothSidewalks=0, pointsWithLeftSidewalk=0, pointsWithRightSidewalk=0, pointsWithNoSidewalks=0;
   const bbox = turf.bbox(road)
   bbox[0]-=0.001    //expand bbox by 0.001 ~ 100m
   bbox[1]-=0.001
@@ -67,7 +67,7 @@ for (let road of roads) {
     let pointHasRightSidewalk=false
     loop1:
     for(let footway of nearby) {
-      if(footway.geometry.type=="MultiLineString")    
+      if(footway.geometry.type=="MultiLineString")
       {
         for(let coords of footway.geometry.coordinates) {
           if(isSidewalkCloseEnough(coords, ptNext)) {
@@ -93,20 +93,18 @@ for (let road of roads) {
     }
     if(pointHasLeftSidewalk){pointsWithLeftSidewalk++}
     if(pointHasRightSidewalk){pointsWithRightSidewalk++}
-    if(pointHasLeftSidewalk || pointHasRightSidewalk){pointsWithSidewalk++}
+    if(pointHasLeftSidewalk && pointHasRightSidewalk){pointsWithBothSidewalks++}
+    if(!pointHasLeftSidewalk && !pointHasRightSidewalk){pointsWithNoSidewalks++}
     offset+=kRoadTestStep
   }
   road.properties.points_tested = pointsTotal
-  road.properties.points_with_sidewalk = pointsWithSidewalk
+  road.properties.points_with_both_sidewalks = pointsWithBothSidewalks
   road.properties.points_with_left_sidewalk = pointsWithLeftSidewalk
   road.properties.points_with_right_sidewalk = pointsWithRightSidewalk
   road.properties.length = roadlen
-  if(pointsTotal && pointsWithSidewalk>=pointsTotal*kPointsWithSidewalksThreshold){  //kPointsWithSidewalksThreshold of points have sidewalk nearby -> good
+  if(pointsTotal && pointsWithBothSidewalks>=pointsTotal*kPointsWithSidewalksThreshold){  //kPointsWithSidewalksThreshold of points have sidewalk nearby -> good
     //console.log(road.properties.name, 'has a sidewalk - ',roadlen,'meters long')
-    roadsWithSidewalks.push(road);
-  }
-  else {
-    roadsWithoutSidewalks.push(road)
+    roadsWithBothSidewalks.push(road);
   }
   if(pointsTotal && pointsWithLeftSidewalk>=pointsTotal*kPointsWithSidewalksThreshold){
     roadsWithLeftSidewalks.push(road);
@@ -114,9 +112,12 @@ for (let road of roads) {
   if(pointsTotal && pointsWithRightSidewalk>=pointsTotal*kPointsWithSidewalksThreshold){
     roadsWithRightSidewalks.push(road);
   }
+  if(pointsTotal && pointsWithNoSidewalks>pointsTotal*(1-kPointsWithSidewalksThreshold)){
+    roadsWithNoSidewalks.push(road)
+  }
   if(roadlen > 300 &&
-    pointsWithSidewalk<pointsTotal*kPointsWithSidewalksThreshold &&
-    pointsWithSidewalk>pointsTotal*0.5 &&
+    pointsWithNoSidewalks>pointsTotal*(1-kPointsWithSidewalksThreshold) &&
+    pointsWithNoSidewalks<pointsTotal*0.5 &&
     road.properties.type!="service")
   {
     roadsToSplit.push(road)
@@ -126,6 +127,7 @@ for (let road of roads) {
 function isSidewalkCloseEnough(line, pt){
   const proj = ruler.pointOnLine(line, pt).point
   const dist = ruler.distance(proj,pt)
+  //const dist = turf.pointToLineDistance(pt, line, 'meters')   //more precise but ~20 times slower
   return dist < kPointDistanceNearby
 }
 
@@ -133,17 +135,17 @@ function isPointOnLeft(ptA1, ptA2, ptB){   //returns whether point B is on the l
      return ((ptA2[0] - ptA1[0])*(ptB[1] - ptA1[1]) - (ptA2[1] - ptA1[1])*(ptB[0] - ptA1[0])) > 0;
 }
 
-writer(outRoadsWithSidewalksPath, turf.featureCollection(roadsWithSidewalks))
+writer(outRoadsWithBothSidewalksPath, turf.featureCollection(roadsWithBothSidewalks))
 writer(outRoadsWithLeftSidewalksPath, turf.featureCollection(roadsWithLeftSidewalks))
 writer(outRoadsWithRightSidewalksPath, turf.featureCollection(roadsWithRightSidewalks))
-writer(outRoadsWithoutSidewalksPath, turf.featureCollection(roadsWithoutSidewalks))
+writer(outroadsWithNoSidewalksPath, turf.featureCollection(roadsWithNoSidewalks))
 writer(outRoadsToSplitPath, turf.featureCollection(roadsToSplit))
 
 
-console.log('Roads with sidewalks: ', roadsWithSidewalks.length)
+console.log('Roads with both sidewalks: ', roadsWithBothSidewalks.length)
 console.log('Roads with left sidewalks: ', roadsWithLeftSidewalks.length)
 console.log('Roads with right sidewalks: ', roadsWithRightSidewalks.length)
-console.log('Roads without sidewalks: ', roadsWithoutSidewalks.length)
+console.log('Roads with no sidewalks: ', roadsWithNoSidewalks.length)
 console.log('Roads to split: ', roadsToSplit.length)
 
 console.timeEnd('Time')
