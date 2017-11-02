6faf544d764e6fdf08bd76f43a03a1c8321b5cdf
Yaro <shkvorets@gmail.com>
2017-11-02 10:42:18 -0400

    added json data file for all roads with all tags as of Oct 20

--- extract/extract.js
+++ extract/extract.js
@@ -1,14 +1,18 @@
 const reader = require('geojson-writer').reader
 const writer = require('geojson-writer').writer
 const turf = require('@turf/turf')
-const ruler = require('cheap-ruler')(45.34, 'meters')
+const ruler = require('cheap-ruler')(45.41, 'meters')
 
 //const sourcePath = "ottawa_pedestrian_network.json"   //download ottawa.ca dataset from http://data.ottawa.ca/dataset/pedestrian-network/resource/e098e33a-9046-48da-bbfb-6f4a4dc2c55e
 //const sourcePath = "ottawa_canada_roads.geojson"    //download IMPOSM extract from https://mapzen.com/data/metro-extracts/metro/ottawa_canada/
-const sourcePath = "ottawa_full_roads_trimble.json"    //download IMPOSM extract from https://market.trimbledata.com/#/account/orders
+//const sourcePath = "ottawa_full_roads_trimble.json"    //download IMPOSM extract from https://market.trimbledata.com/#/account/orders
+const sourcePath = "ottawa_full_all_tags_utf8_1.json"
 //const targetPath = "ottawa_central_sidewalks.json"
-const targetPath = "ottawa_full_roads.json"
+const targetPath = "ottawa_full_roads_all_tags_1.json"
 const cityBoundaryPath = "ottawa_boundaries.json"
+
+const kExtractOnlyLines = true
+const kExtractOnlyHighways = true
 
 const collection = []
 
@@ -26,6 +30,8 @@ let id=1;
 loop1:
 for(let feature of features) {
   if(!feature.geometry) continue;
+  if(kExtractOnlyHighways && !feature.properties.highway) continue;
+  if(kExtractOnlyLines && (feature.geometry.type!="LineString" && feature.geometry.type!="MultiLineString")) continue;
   for(const point of turf.explode(feature).features) {
     if(!ruler.insideBBox(point.geometry.coordinates, bbox) || !turf.inside(point,city)){  //not inside bbox and not inside city
         continue loop1
